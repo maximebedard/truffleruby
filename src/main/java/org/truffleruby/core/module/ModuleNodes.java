07bbf1c2557be5d7d81292f2868bdbd47e2e9a5f
Benoit Daloze <eregontp@gmail.com>
2018-01-03 15:44:33 +0100

    cleanup

--- src/main/java/org/truffleruby/core/module/ModuleNodes.java
+++ src/main/java/org/truffleruby/core/module/ModuleNodes.java
@@ -1898,11 +1898,7 @@ public abstract class ModuleNodes {
                 throw new RaiseException(coreExceptions().typeErrorWrongArgumentType(classToRefine, "Class", this));
             }
 
-//            InternalMethod method = getContext().getCallStack().getCallingMethodIgnoringSend();
-//            LexicalScope lexicalScope = method == null ? null : method.getSharedMethodInfo().getLexicalScope();
-
             ConcurrentMap<DynamicObject, DynamicObject> refinements = Layouts.MODULE.getFields(self).getRefinements();
-//            ConcurrentMap<DynamicObject, DynamicObject> activatedRefinements = Layouts.MODULE.getFields(self).getActivatedRefinements();
 
             DynamicObject refinement = refinements.get(classToRefine);
             if (refinement == null) {
